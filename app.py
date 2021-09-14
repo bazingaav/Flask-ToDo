e0dea04099901bc4815be2cddea306910e72f205
Athashree Vartak <athashreeav5556@gmail.com>
2021-09-13 22:26:01 -0700

    DB Struct

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
+from flask_sqlalchemy import SQLAlchemy
 
 #Convention
 app = Flask(__name__)
+#Path for DB -/// -> relative path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#DB structure
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500))
+    isComplete = db.Column(db.Boolean)
 
 #Define index page
 @app.route('/')
@@ -15,4 +26,5 @@ def about():
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
